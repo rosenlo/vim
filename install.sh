e9a71cf6d220e3fb170f46f19a173bcc72ceae7d
罗子豪 <rosenluov@gmail.com>
2016-12-24 16:53:19 +0800

    Add autoload

--- install.sh
+++ install.sh
@@ -13,4 +13,5 @@ ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
 /usr/bin/env git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
+mkdir -p ~/.vim/autoload && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
